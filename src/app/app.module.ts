b762c25803c49401d45ba7d9e56ebdb7c594eda5
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2023-07-05 22:45:32 +0100

    make some modifications in dashbaord

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,12 @@ import {AppComponent} from './app.component';
 import {ProductListComponent} from './shared/components/products/product-list/product-list.component';
 import {AdminPanelComponent} from './admin/components/admin-panel/admin-panel.component';
 import {TagModule} from "primeng/tag";
-import {DropdownModule} from "primeng/dropdown";
 import {FormsModule} from "@angular/forms";
+import {DropdownModule} from "primeng/dropdown";
 import {FileUploadModule} from "primeng/fileupload";
 import {SliderModule} from "primeng/slider";
 import {MultiSelectModule} from "primeng/multiselect";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -27,6 +28,7 @@ import {MultiSelectModule} from "primeng/multiselect";
     AdminPanelComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
@@ -39,6 +41,7 @@ import {MultiSelectModule} from "primeng/multiselect";
     FileUploadModule,
     SliderModule,
     MultiSelectModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
